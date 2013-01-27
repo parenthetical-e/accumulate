eb6ef9dc7339f90f5d4f7193233744c005258a2d
Erik Peterson <Erik.Exists@gmail.com>
2013-01-27 16:10:01 -0500

    sim.test.SelecTrials() is now working as expecting.  max_trial_count is now set correctly.  Also trial generation was moved to a separate function, this allows max_ to be set and may ease future development.

--- bin/test.py
+++ bin/test.py
@@ -1,6 +1,6 @@
 """ Top-level control for test/experimental functions. """
 import pprint
-from accumulate.sim.base import Trials
+from accumulate.sim.test import SelectTrials
 from accumulate.stats import (divergence, divergence_by_trial, 
         correct, reaction_time_difference, mean_rt)
 from accumulate.models import construct
@@ -14,26 +14,29 @@ def main(name, l, threshold, decider):
     # Take the constructed models, and any param free models
     # add them all the the list of models to run, i.e. to 
     # use to categorize the trials in Trials.
-    info = construct.create_information(threshold, decider)
-    lratio = construct.create_likelihood_ratio(threshold, decider)
-    absc = construct.create_abscount(threshold, decider)
-    relc = construct.create_relcount(threshold, decider)
-    naive = construct.create_naive_probability(threshold, decider)
-    snr = construct.create_snr(threshold, decider)
-    gating = construct.create_urgency_gating(threshold, decider, gain=0.4)
-    lba = construct.create_incremental_lba(threshold, decider, k=0.1, d=0.1)
-    blca = construct.create_blca(
-            threshold, decider, length=10, k=0.1, wi=0.1, leak=0.1, beta=0.1)
+    # info = construct.create_information(threshold, decider)
+    lratio = construct.create_likelihood_ratio(
+            "lratio1", threshold, decider)
+    # absc = construct.create_abscount(threshold, decider)
+    # relc = construct.create_relcount(threshold, decider)
+    # naive = construct.create_naive_probability(threshold, decider)
+    # snr = construct.create_snr(threshold, decider)
+    gating1 = construct.create_urgency_gating(
+            "gating_04", threshold, decider, gain=0.4)
+    # lba = construct.create_incremental_lba(threshold, decider, k=0.1, d=0.1)
+    # blca = construct.create_blca(
+    #         threshold, decider, length=10, k=0.1, wi=0.1, leak=0.1, beta=0.1)
 
     # TODO - rest of models not here, 
     # but for leaky also do one with
     # comp at zero (with leak) and seperatly one 
     # with leak at 0 (with comp valued)
     
-    run_models = [info, lratio, absc, relc, naive, snr, gating, lba, blca]
+    run_models = [gating1, lratio]
+    # run_models = [info, lratio, absc, relc, naive, snr, gating, lba, blca]
     # print(run_models)
     # Create the experimental instance
-    exp = Trials(l)
+    exp = SelectTrials(l)
 
     # Loop over each model and categorize it.
     results_list = list()
@@ -72,8 +75,5 @@ def main(name, l, threshold, decider):
     
     
 if __name__ == "__main__":
-    main('l8_abs', 8, 0.65, absolute)
-    #main('l14_abs', 14, 0.65, absolute)
-
-    main('l8_diff', 8, 0.65, difference)
-    #main('l4_diff', 14, 0.65, difference)
+    main('l8_050_abs', 8, 0.5, absolute)
+    main('l8_050_diff', 8, 0.5, difference)
--- sim/test.py
+++ sim/test.py
@@ -1,29 +1,50 @@
 """ Classes from base modified to ease testing. """
 from accumulate.sim.base import Trials
 
+
 class SelectTrials(Trials):
     """ Experiment on a set of select trials whose modeled results should be 
     intuitive, or otherwise easy to analyze for correctness and 
-    consistency. """
-    
+    consistency. """    
     
     def __init__(self, l):
         Trials.__init__(self, l)
+
+        # Over ride max_trial_count...        
+        self.max_trial_count = len(self._get_test_trials())
+            ## Calling _get_test_trials()
+            ## is inefficient (as it always called below too) but, well,
+            ## it ensures max_trial_count is over-ridden correctly
+            ## without use intervention.  I expect _get_test_trials()
+            ## will be tweaked often during development.
     
     
-    def _generate_trials():
+    def _get_test_trials(self):
+        """As is says on the label, get em. """
+        
+        l = int(self.l)
+        test_trials = [
+            ["A"] * l,  ## All A or B
+            ["B"] * l,
+            ["B"] + ["A"] * (l-1),  ## One then rest
+            ["A"] + ["B"] * (l-1),
+            ["A"] * (l/2) + ["B"] * (l/2),  ## Half and half
+            ["B"] * (l/2) + ["A"] * (l/2),
+            'AB' * (l/2),  ## Alternating A/B
+            'BA' * (l/2)
+        ]
+        
+        return test_trials
+    
+    
+    def _generate_trials(self):
         """ Returns a generator of select testing trials. """
     
         self.trial_count = 0
             ## reset
         
-        test_trials = [
-            ["A"] * l,
-            ["B"] * l,
-            ["A"] * l/2 + ["B"] * l/2
-            ["B"] * l/2 + ["A"] * l/2
-        ]
+        test_trials = self._get_test_trials()
         
         return (t for t in test_trials) 
-            ## A Generator expression
-            
+            ## Returns a generator expression
+
